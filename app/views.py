d08fd69a4c4b97fe17cf9e48e2ef0dfab6268c17
nicko17 <104779079+nicko17@users.noreply.github.com>
2022-06-15 23:18:51 -0500

    errores

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from .forms import *
 from .models import *
 
