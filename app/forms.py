a5af7466c99c1178a9309f99cf7f49619e6968ef
vector18xd <victxd18pepe@gmail.com>
2022-07-21 15:49:27 -0500

    Modulo Completo Horarios

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,10 @@
 from dataclasses import field, fields
+from pyexpat import model
 from django import forms
 from django.forms import ValidationError
 from .models import *
 
 class RegistrationForm(forms.ModelForm):
-
-    
-
     class Meta:
         model = Trabajadores
         fields = ["nombres", "apellidos", "telefono", "email", "foto", "password", "nom_local", "direccion", "idCategoria"]
@@ -16,9 +14,6 @@ class EditarClienteForm(forms.ModelForm):
         model = Clientes
         fields = ["nombres", "apellidos", "telefono", "foto"]
 class EditarBarberoForm(forms.ModelForm):
-    class Meta:
-        model = Trabajadores
-        fields = ["nombres", "apellidos", "telefono", "foto", "idCategoria", "nom_local", "direccion"]
     class Meta:
         model = Trabajadores
         fields = ["nombres", "apellidos", "telefono", "foto", "nom_local", "direccion", "idCategoria"]
@@ -32,3 +27,8 @@ class Citas(forms.ModelForm):
     class Meta:
         model = citas
         fields = ["idServicio","horaRegistroCita", "fechaRegistroCita", "idHorario"]
+        
+class EditarHorarios(forms.ModelForm):
+    class Meta:
+        model = horarios
+        fields = ["horaInicio","fecha"]
